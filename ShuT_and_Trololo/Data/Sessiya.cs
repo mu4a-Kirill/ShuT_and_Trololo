b0ee9e98dd9515025348707ca6abcec10daf0177
Кирилл Кузнецов <bossshark06@gmail.com>
2026-05-26 22:43:48 +0300

    доработка БД, модели данных, класс сессии

---
+++ ShuT_and_Trololo/Data/Sessiya.cs
@@ -0,0 +1,25 @@
+﻿using ShuT_and_Trololo.Models;
+
+namespace ShuT_and_Trololo.Data
+{
+    public static class Sessiya
+    {
+        public static Polzovatel TekushiyPolzovatel { get; set; }
+
+        public static bool EstPolzovatel => TekushiyPolzovatel != null;
+
+        public static bool EtoAdmin =>
+            TekushiyPolzovatel?.RoleName == "Администратор";
+
+        public static bool EtoAvtor =>
+            TekushiyPolzovatel?.RoleName == "Автор";
+
+        public static bool AkkZamorozhen =>
+            TekushiyPolzovatel?.IsFrozen == true;
+
+        public static void Viyti()
+        {
+            TekushiyPolzovatel = null;
+        }
+    }
+}
---
+++ ShuT_and_Trololo/Models/Polzovatel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuT_and_Trololo.Models
+{
+    public class Polzovatel
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool IsFrozen { get; set; }
+        public string FreezeReason { get; set; }
+        public string AvatarPath { get; set; }
+        public string About { get; set; }
+    }
+}
